d453cead7c9efcc2a64f65b84f57e2071af7d07b
zaineb <zaineb.bachouch@gmail.com>
2019-03-14 12:16:07 +0100

    division page index.php

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,8 @@
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
  <div class="sidebar-module sidebar-module-inset">
- <h4>About</h4>
-<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis
-consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed
-consectetur.</p>
+ 
+<h4>About</h4> <p><?php the_author_meta( 'description' ); ?>
+</p> 
  </div>
  <div class="sidebar-module">
  <h4>Archives</h4> <ol class="list-unstyled"> <?php
