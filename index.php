69cba35ae6d88a4651a74168d4ea5a2e291e2e53
zaineb <zaineb.bachouch@gmail.com>
2019-03-14 11:56:38 +0100

    division page index.php

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <?php get_header(); ?>
 <div class="row">
  <div class="col-sm-8 blog-main">
- <?php get_header(); ?>
-<div class="row">
- <div class="col-sm-8 blog-main">
- <?php if ( have_posts() ) : while ( have_posts() ) :
-the_post(); get_template_part( 'content', get_post_format() );
-endwhile; endif; ?> </div> <!-- /.blog-main -->
- <?php get_sidebar(); ?> </div> <!-- /.row -->
-<?php get_footer(); ?>
+ 
+<?php if ( have_posts() ) : while ( have_posts() ) :
+the_post(); ?>
  <?php get_template_part( 'content', get_post_format() ); ?>
+<?php endwhile; endif; ?>
  </div> <!-- /.blog-main -->
  <?php get_sidebar(); ?>
 </div> <!-- /.row -->
