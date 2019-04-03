dd2a607a04eb1670fa5e468226035a168eb3b4ba
zaineb <zaineb.bachouch@gmail.com>
2019-04-03 21:37:49 +0100

    devwoedpress

--- content.php
+++ content.php
@@ -1,6 +1,11 @@
 <div class="blog-post">
- <h2 class="blog-post-itle"><?php the_title(); ?></h2>
+ <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> 
   <p class="blog-post-meta"><?php the_date(); ?> par 
   <a href="#"><?php the_author(); ?></a></p>
  <?php the_content(); ?>
 </div><!-- /.blog-post -->
+ <?php the_excerpt(); ?>
+ <?php if ( has_post_thumbnail() ) {?> 
+<div class="row">  <div class="col-md-4"> <?php the_post_thumbnail('thumbnail'); ?> 
+</div> 
+ <div class="col-md-6"> <?php the_excerpt(); ?> </div> </div>  <?php } else { ?>  <?php the_excerpt(); ?> <?php } ?>
--- index.php
+++ index.php
@@ -5,8 +5,11 @@
 <?php if ( have_posts() ) : while ( have_posts() ) :
 the_post(); ?>
  <?php get_template_part( 'content', get_post_format() ); ?>
-<?php endwhile; endif; ?>
+<?php endwhile;?>
+<nav>   <ul class="pager">  <li><?php next_posts_link( 'Previous' ); ?></li>  <li><?php previous_posts_link( 'Next' ); ?></li>   </ul> </nav> 
+ <?php endif; ?>
  </div> <!-- /.blog-main -->
  <?php get_sidebar(); ?>
 </div> <!-- /.row -->
 <?php get_footer(); ?>
+<nav>   <ul class="pager">  <li><?php next_posts_link( 'Previous' ); ?></li>  <li><?php previous_posts_link( 'Next' ); ?></li>   </ul> </nav> 
